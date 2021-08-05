2274fda583f8a83dfdf016c1fcd8de7b0e2baeeb
haan823 <bejimil@g.skku.edu>
2021-08-05 21:26:07 +0900

    Posts Entity 생성

---
+++ src/main/java/com/project/bug_report/domain/posts/Posts.java
@@ -0,0 +1,19 @@
+package com.project.bug_report.domain.posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
