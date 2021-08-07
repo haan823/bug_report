56ac1dd8c2c2146ffc89f7c31358ad83c404b443
haan823 <bejimil@g.skku.edu>
2021-08-07 23:06:38 +0900

    Posts Entity 수정

--- src/main/java/com/project/bug_report/domain/posts/Posts.java
+++ src/main/java/com/project/bug_report/domain/posts/Posts.java
@@ -1,12 +1,10 @@
 package com.project.bug_report.domain.posts;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
@@ -16,4 +14,20 @@ public class Posts {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 }
