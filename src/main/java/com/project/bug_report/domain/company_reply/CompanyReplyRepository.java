40d734b51f4a286c68e8f55ccef9e857fc5cc752
SANGHYUN KIM <haan823@naver.com>
2021-08-11 19:28:01 +0900

    [ADD] Added CompanyReplyRepository

---
+++ src/main/java/com/project/bug_report/domain/company_reply/CompanyReplyRepository.java
@@ -0,0 +1,6 @@
+package com.project.bug_report.domain.company_reply;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyReplyRepository extends JpaRepository<CompanyReply, Long> {
+}
