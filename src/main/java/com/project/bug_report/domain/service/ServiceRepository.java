3cf7c7a4df5da93ae652a8f499d77018dda21c2b
SANGHYUN KIM <haan823@naver.com>
2021-08-11 19:31:50 +0900

    [ADD] Added ServiceRepository

---
+++ src/main/java/com/project/bug_report/domain/service/ServiceRepository.java
@@ -0,0 +1,6 @@
+package com.project.bug_report.domain.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+}
